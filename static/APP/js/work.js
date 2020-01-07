63d2b9e6e74cc3fc0ff6cc245f4667f1f7a7bdf9
ourvibes <cr1124@outlook.com>
2020-01-07 21:30:11 +0800

    update

--- static/APP/js/work.js
+++ static/APP/js/work.js
@@ -1,35 +1,38 @@
-
 this.addEventListener('message', (e) => {
   const T = (to = {}) => {
     let { name, url } = to
     if (name && url) {
-      return `<div class="col-2 col-offset-2"><span class="icon-leaf"></span>&ensp;<span>${name}</span></div><div class="col-4 col-offset-2"><a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a></div>`
+      return `<div class="col-2 col-offset-2"><span class="icon-leaf"></span> <span>${name}</span></div><div class="col-4 col-offset-2"><a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a></div>`
     }
   }
 
   const handleData = (data = {}) => {
     const { ip, list = [] } = data
+    const format = (item) => {
+      if (!item._key) {
+        item._key = item.key.toLowerCase()
+      }
+      if (item.url && item.url.indexOf('{ip}') > -1) {
+        item.url = item.url.replace(/{ip}/g, ip)
+      }
+      if (!item.innerHTML) {
+        item.innerHTML = T(item)
+      }
+    }
     list
       .sort((x, y) => {
-        x = x.key.toLowerCase()
-        y = y.key.toLowerCase()
-        if (x > y) {
+        format(x)
+        format(y)
+        if (x._key > y._key) {
           return 1
-        } else if (x < y) {
+        } else if (x._key < y._key) {
           return -1
         } else {
           return 0
         }
-      })
-      .map((it) => {
-        it.url = it.url && it.url.replace(/{ip}/g, ip)
-        it.innerHTML = T(it)
       })
     return data
   }
 
   postMessage(handleData(e.data))
 })
-
-
-
